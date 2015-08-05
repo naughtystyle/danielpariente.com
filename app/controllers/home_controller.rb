a05c3ce129001b4953434afab15fe4fd3cab6be5
Dan Pariente <dansification@gmail.com>
2015-08-04 22:32:52 -0400

    Update Information
    * Add Contact email
    * Update Resume
    * Change Profile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,14 @@ class HomeController < ApplicationController
   def index
   end
 
+  def resume
+    send_file(
+      "#{Rails.root}/app/assets/files/resume.pdf",
+      filename: "resume.pdf",
+      type: "application/pdf"
+    )
+  end
+
   def flashcards
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+  get 'contact', to: 'home#contact', as: 'contact'
   get 'flashcards', to: 'home#flashcards', as: 'flashcards'
+  get 'resume', to: 'home#resume', as: 'resume'
 
 end
